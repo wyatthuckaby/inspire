1683e5c23527fe7cf774440a6e402ff52a59e6b0
Wyatt Huckaby <huckabywyatt@gmail.com>
2017-08-11 10:56:43 -0600

    server now listens to multiport requests.

---
+++ config/server/local.js
@@ -0,0 +1,3 @@
+exports.ip = "127.0.0.1";
+exports.portStart = 3000;
+exports.portEnd = 3010; //keep this the same as portstart if you want singular porting.
--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-//require your dependencies
+/**
+ * INDEX.JS
+ * 
+ * CORE SERVER FILE
+ */
 
 
 
-//tell your server what it needs to use
+var express = require('express');
+var bodyParser = require('body-parser');
+var serverConf = require('./config/server/local.js');
+var server = express();
 
 
-
-//register your todos routes as api/routes
-
+server.use(express.static(__dirname + "/www"));
 
 
-//start your server listening....
+/**
+ * SERVER LISTENER
+ */
+
+//user is attempting to use multiport mode.
+//create the correct amount of sockets to accomidate them.
+if (serverConf.portStart < serverConf.portEnd) {
+    for (var sport = serverConf.portStart; sport < serverConf.portEnd; sport++) {
+
+        server.listen(sport, serverConf.ip, function() {
+            //Success
+            console.log(`Socket - [ OK ]`);
+        }).on('error', function(err) {
+            //fail
+            console.log(err);
+            console.log(`Socket - [FAIL]`);
+        });
+    }
+    console.log(`SERVER LISTENING - BOUND ON ${serverConf.ip} ports ${serverConf.portStart} - ${serverConf.portEnd} `);
+    console.log(`Multiport mode enabled on range`)
+
+
+}
+//single socket mode. 
+else if (serverConf.portStart === serverConf.portEnd) {
+    server.listen(serverConf.portStart, serverConf.ip, function() {
+        //success
+        console.log(`SERVER LISTENING - BOUND ON ${serverConf.ip} port ${serverConf.portStart}`);
+    }).on("error", function(err) {
+        //failure
+        console.log(`Failed to create socket. This port might be used.${err}`);
+    });
+}
+//fail
+else {
+    console.log("FATAL: Port end cannot be lower than the start");
+}
